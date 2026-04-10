fde8941911dfb49a05cdaca158bc03c1637c6ba3
ST10399194 <ST10399194@rcconnect.edu.za>
2026-04-10 15:27:26 +0200

    AudioPlayer comments added

--- AudioPlayer.cs
+++ AudioPlayer.cs
@@ -6,21 +6,25 @@ namespace ChatBotApp
 {
     public class AudioPlayer
     {
+        //plays audio file
         public static void PlayGreeting(string filePath = "greeting.wav")
         {
+            //checks if the sudio file exists
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($"(Audio file not found: {filePath})");
-                return;
+                return; //exit early if the file is missing
             }
 
             try
             {
+            // create a soundplayer instance for the given file
                 using var player = new SoundPlayer(filePath);
-                player.Play(); // Or player.Play() for async
+                player.PlaySync(); 
             }
             catch (InvalidOperationException ex)
             {
+                //handles errors if the audio desnt play
                 Console.WriteLine($"Audio error: {ex.Message}");
             }
         }
