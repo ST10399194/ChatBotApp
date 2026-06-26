f0ef13e23d2ee61c5eb4df89326b92d78465380e
ST10399194 <ST10399194@rcconnect.edu.za>
2026-06-26 17:37:19 +0200

    Create QuizQuestion model class and QuizManager logic engine with questions

---
+++ CybersecurityChatbot/QuizQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybersecurityChatbot
+{
+      public class QuizQuestion
+        {
+            public string Question { get; set; }
+            public List<string> Options { get; set; } // A, B, C, D for multiple choice
+            public string CorrectAnswer { get; set; }  // e.g., 'C' or 'True'
+            public string Explanation { get; set; }   // shown after answering
+            public bool IsTrueFalse { get; set; }      // differentiates display mode
+        }
+    }
+
+
+
