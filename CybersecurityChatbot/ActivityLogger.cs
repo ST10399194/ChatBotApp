a5fe673a5ccb7bab38b2be9c211c59ebf487f24f
ST10399194 <ST10399194@rcconnect.edu.za>
2026-06-26 18:38:13 +0200

    Add ActivityLogger class

---
+++ CybersecurityChatbot/ActivityLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybersecurityChatbot
+{
+    public static class ActivityLogger
+    {
+        private static readonly List<string> _logs = new List<string>();
+
+        public static void Log(string action)
+        {
+            _logs.Add(action);
+        }
+
+        public static string GetRecentLog()
+        {
+            if (_logs.Count == 0)
+                return "No recent actions recorded yet.";
+
+            StringBuilder sb = new StringBuilder("Here's a summary of recent actions:\n");
+            for (int i = 0; i < _logs.Count; i++)
+            {
+                sb.AppendLine($"{i + 1}. {_logs[i]}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
